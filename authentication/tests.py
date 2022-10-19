9553c2d52ddf982b34b78722668a2b8ce2721002
Azzuhri Nauli Pasaribu <azzuhri.nauli@ui.ac.id>
2022-10-19 16:51:26 +0700

    Add failing tests for authentication (Red)

---
+++ authentication/tests.py
@@ -0,0 +1,58 @@
+from django.test import TestCase, Client
+from django.urls import reverse, resolve
+
+
+# Create your tests here.
+class TestUrls(TestCase):
+    def test_register_url_is_resolved(self):
+        url = reverse('register')
+        self.assertEqual(resolve(url).func, register)
+        
+    
+    def test_login_url_is_resolved(self):
+        url = reverse('login')
+        self.assertEqual(resolve(url).func, login)
+
+    def test_logout_url_is_resolved(self):
+        url = reverse('logout')
+        self.assertEqual(resolve(url).func, logout)
+
+class TestViews(TestCase):
+    def setUp(self) -> None:
+        self.client = Client()
+
+    def test_register_GET(self):
+        response = self.client.get(reverse('register'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'register.html')
+
+    def test_register_POST(self):
+        response = self.client.post(reverse('register'), {
+            'username':'JohnDoe',
+            'password':'Example1.'
+        })
+
+        self.assertEqual(response.status_code, 302)
+    
+    def test_login_GET(self):
+        self.client = Client(enforce_csrf_checks=True)
+        response = self.client.get(reverse('login'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'login.html')
+
+    def test_register_POST(self):
+        response = self.client.post(reverse('login'), {
+            'username':'JohnDoe',
+            'password':'Example1.'
+        })
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_logout(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 302)
+        
+class TestForms(TestCase):
+    pass
