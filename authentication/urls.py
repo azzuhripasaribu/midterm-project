4080edc66f08ad1a1093f0a0a92709a4a5127b45
Azzuhri Nauli Pasaribu <azzuhri.nauli@ui.ac.id>
2022-12-12 21:05:41 +0700

    fixed urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,6 @@
 from telnetlib import LOGOUT
 from django.urls import path
-from authentication.views import register_user, login_user, logout_user
+from authentication.views import register_user, login_user, logout_user, login
 
 
 app_name = 'authentication'
@@ -9,4 +9,5 @@ urlpatterns = [
     path('register',register_user, name='register'),
     path('login',login_user, name='login'),
     path('logout', logout_user, name='logout'),
+    path('json'), 
 ]
